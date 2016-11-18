9aae10b4a3837066cdeef8b9e6bc42971ba5124f
Ryusuke Kawasaki <kawasaki@codable.co.jp>
2016-11-18 14:29:08 +0900

    first commit

---
+++ FirstiOSApp/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  FirstiOSApp
+//
+//  Created by 川崎 隆介 on 2015/10/13.
+//  Copyright (c) 2015年 川崎 隆介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITextFieldDelegate {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
+    var userName = "";
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func changeName(_ sender: Any) {
+        if(self.userName.characters.count > 0){
+            self.label.text = "こんにちは、" + self.userName + "さん"
+        }else{
+            self.label.text = "名前が入力されていません"
+        }
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if(textField == self.textField){
+            self.userName = textField.text!
+            textField.resignFirstResponder()
+        }
+        return true;
+    }
+    
+}
+
